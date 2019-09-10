b0ff9a54819a3f1d78bc0f1a0fc92da9be68a200
Marko Stojanovic (E-Search) <v-mastoj@microsoft.com>
2019-09-10 17:44:38 +0200

    Init commit. Singleton stateles svc

---
+++ FrontEndSvc/Controllers/FrontEndSvcController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEndSvc.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/WordCount")]
+    public class FrontEndSvcController : Controller
+    {
+        private readonly HttpClient httpClient;
+        private readonly StatelessServiceContext context;
+        private readonly FabricClient fabricClient;
+
+        public FrontEndSvcController(HttpClient httpClient, StatelessServiceContext context, FabricClient fabricClient)
+        {
+            this.httpClient = httpClient;
+            this.context = context;
+            this.fabricClient = fabricClient;
+        }
+
+        // Post: api/WordCount
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string sourceText)
+        {
+            ServiceEventSource.Current.ServiceMessage(this.context, "Received param: " + sourceText);
+            MatchCollection collection = Regex.Matches(sourceText, @"\b\w+\b");
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            foreach(Match match in collection)
+            {
+                string s = match.Value.ToLower();
+                if(counts.ContainsKey(s))
+                {
+                    counts[s]++;
+                }
+                else
+                {
+                    counts[s] = 1;
+                }
+
+            }
+
+            return Json(counts);
+        }
+    }
+}
---
+++ WordCount/pkg/Debug/FrontEndSvcPkg/Code/wwwroot/js/site.js
@@ -0,0 +1,49 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var app = angular.module('WordCountApp', ['ui.bootstrap']);
+app.run(function () { });
+
+app.controller('WordCountController', ['$rootScope', '$scope', '$http', '$timeout', function ($rootScope, $scope, $http, $timeout) {
+
+    $scope.process = function (sourceText) {
+        console.log("Called shit");
+        $http.post('api/WordCount', "\"" + sourceText + "\"")
+            .then(function (data, status) {
+                ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "b55b025e438fa8a98e32482b5f768ff5"];
+                zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
+                var myConfig = {
+                    "graphset": [
+                        {
+                            "type": "wordcloud",
+                            "options": {
+                                "style": {
+                                    "tooltip": {
+                                        visible: true,
+                                        text: '%text: %hits'
+                                    }
+                                },
+                                "words": []
+                            }
+                        }
+                    ]
+                };
+
+                for (let word in data["data"]) {
+                    myConfig["graphset"][0]["options"]["words"].push({ "text": word, "count": data["data"][word] });
+                }
+
+                zingchart.render({
+                    id: 'chart-container',
+                    data: myConfig,
+                    height: '100%',
+                    width: '100%'
+                });
+            }, function (data, status) {
+                console.log("No data received");
+            });
+    };
+
+}]);
